2f446c4d4758aa94b23ed2611989f3a91bf57473
JulioSquire <jcmorillo444@gmail.com>
2024-11-26 17:12:12 -0500

    Added Entities and Sprites files
    Video timestamp:
    29:00

---
+++ Monster Files/start project/code (start)/entities.py
@@ -0,0 +1,8 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.surface((100,100))
+        self.image('red')
+        self.rect = self.image.get_frect(center = pos)
--- Monster Files/start project/code (start)/settings.py
+++ Monster Files/start project/code (start)/settings.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.math import Vector2 as vector 
+from pygame.math import Vector2 as vector
 from sys import exit
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
